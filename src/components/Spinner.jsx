584b915c82e59d5b612163c1a79d9803853ec93f
clauwaesenne <clauwaesenne@gmail.com>
2024-04-29 19:18:27 +0200

    Rijen van producten en search

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,7 @@
+import Spinner from "react-bootstrap/Spinner";
+
+function SpinnerAnimation() {
+  return (<Spinner animation="grow" variant="danger" />);
+}
+
+export default SpinnerAnimation;
--- src/pages/admin/inventaris.jsx
+++ src/pages/admin/inventaris.jsx
@@ -8,28 +8,41 @@ import { IoMdArrowDropdown } from "react-icons/io";
 import axios from "axios";
 import { data } from "autoprefixer";
 import canonFoto from "../../assets/canon-eos-200d.jpg";
+import Spinner from "../../components/Spinner";
+import { HiMiniPencilSquare } from "react-icons/hi2";
+import { FaCircleInfo } from "react-icons/fa6";
 
 const Inventaris = () => {
   const [productModellen, setProductModellen] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     setLoading(true);
-    fetch("http://localhost:8080/productmodel")
-      .then((response) => response.json()) // Parse JSON data here
-      .then((data) => {
-        setProductModellen(data); 
-        console.log(data)// Set state with parsed data
+    axios
+      .get("http://localhost:8080/productmodel")
+      .then((response) => {
+        setProductModellen(response.data);
+        console.log(data);
         setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching data: ", error);
         setLoading(false);
       });
-  }, []); // Empty dependency array to run effect only once on component mount
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredProductModellen = productModellen.filter((model) =>
+    model.productModelNaam.toLowerCase().includes(searchQuery.toLowerCase())||
+    model.productModelMerk.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
-    <content className="flex top-0 flex-grow">
+    <content className="top-0 flex-grow">
       <main className="flex-grow p-10">
         <div className="flex justify-between items-center">
           <h1 className=" flex text-3xl font-bold w-40 border-b justify-center">
@@ -57,6 +70,8 @@ const Inventaris = () => {
                 id=""
                 placeholder="Zoek hier"
                 className="h-full w-full rounded-xl"
+                value={searchQuery}
+                onChange={handleSearch}
               />
             </div>
             <div className="flex h-full border-2 items-center justify-center gap-3 rounded-xl border-Lichtgrijs w-28 hover:cursor-pointer">
@@ -66,94 +81,87 @@ const Inventaris = () => {
           </div>
         </div>
         <div className="flex w-full h-screen">
-          <table className="w-full mt-4 h-screen  ">
+          <div>{loading && <Spinner />}</div>
+          <table className="w-full h-screen">
             <thead className="w-full h-16">
-              <tr className="text-sm text-Grijs font-thin ">
-                <th
-                  scope="col"
-                  className="py-3 px-6 w-hover:cursor-pointer "
-                >
+              <tr className="text-sm text-Lichtgrijs font-thin">
+                <th scope="col" className=" px-2 ">
                   Nr
                 </th>
                 <th
                   scope="col"
-                  className="py-3 px-6 hover:cursor-pointer"
+                  className="text-left hover:cursor-pointer w-80 "
                 >
                   Product
                 </th>
-                <th scope="col" className="py-3 px-6">
+                <th scope="col" className="text-left">
                   Categorie
                 </th>
-                <th scope="col" className="py-3 px-6">
+                <th
+                  scope="col"
+                  className=" flex h-full justify-center items-center  gap-1"
+                >
                   Beschikbaar
-                  <IoMdArrowDropdown className="size-4" />
+                  <IoMdArrowDropdown className="size-4 text-Grijs" />
                 </th>
-                <th scope="col" className="py-3 px-6">
+                <th scope="col" className="">
                   Uitgeleend
                 </th>
-                <th scope="col" className="py-3 px-6">
+                <th scope="col" className=" ">
                   Gereserveerd
                 </th>
-                <th scope="col" className="py-3 px-6">
+                <th scope="col" className=" ">
                   Gepauzeerd
                 </th>
-                <th scope="col" className="py-3 text-right">
+                <th scope="col" className="">
                   Actie
                 </th>
               </tr>
             </thead>
             <tbody>
-              {productModellen.map((model) => (
-                <tr
-                  key={model.productModelNr}
-                  className="border"
-                >
-                  <td className="flex justify-center">{model.productModelNr}</td>
-                  <td className=" items-center">
-                    <div className="flex h-full item-center">
-                      <div className="h-16 auto">
-                      <img
-                        src={canonFoto}
-                        alt=""
-                        className=""
-                      />
+              {filteredProductModellen.map((model) => (
+                <tr key={model.productModelNr} className="h-16">
+                  <td className="flex justify-center items-center h-full">
+                    {model.productModelNr}
+                  </td>
+                  <td className="">
+                    <div className="flex items-center justify-start gap-2 overflow-x-hidden">
+                      <div className="h-12 w-12 overflow-hidden rounded-full border-2 border-Lichtgrijs">
+                        <img
+                          src={canonFoto}
+                          alt=""
+                          className="w-full h-full object-cover bg-white"
+                        />
                       </div>
-                      <h2 className="flex flex-col">
-                        <span>{`${model.productModelMerk}`}</span>
-                        <span>{`${model.productModelNaam}`}</span>
+                      <h2 className="flex flex-col -space-y-1">
+                        <span className="text-base text-black font-semibold">{`${model.productModelNaam}`}</span>
+                        <span className="text-sm text-Lichtgrijs">{`${model.productModelMerk}`}</span>
                       </h2>
                     </div>
                   </td>
                   <td className="">
-                    <div>
-                      <h2>{model.categorie}</h2>
+                    <div className="flex w-full border h-8 bg-slate-300 items-center justify-center rounded-md ">
+                      Video
                     </div>
                   </td>
+                  <td className="">{model.beschikbaar}</td>
+                  <td className="">{model.uitgeleend}</td>
+                  <td className="">{model.gereserveerd}</td>
+                  <td className="">{model.gepauzeerd}</td>
                   <td className="">
-                    {model.beschikbaar}
-                  </td>
-                  <td className="">
-                    {model.uitgeleend}
-                  </td>
-                  <td className="">
-                    {model.gereserveerd}
-                  </td>
-                  <td className="">
-                    {model.gepauzeerd}
-                  </td>
-                  <td className="">
-                    <div>
+                    <div className="flex justify-end items-center gap-2">
                       <Link
                         to={`/admin/inventaris/wijzigen/${model.productModelNr}`}
-                        className="bg-red-500 hover:bg-red-900 text-white py-2 px-4 rounded-lg"
+                        className="bg-cyan-600 text-white py-1 px-1 rounded-xl flex items-center justify-center hover:bg-cyan-900"
                       >
-                        wijzig
+                        <HiMiniPencilSquare className="size-6" />
                       </Link>
                       <Link
                         to={`/admin/inventaris/info/${model.productModelNr}`}
-                        className="bg-red-500 hover:bg-red-900 text-white py-2 px-4 rounded-lg"
+                        className="bg-Grijs text-white py-1 px-1 rounded-xl flex items-center justify-center hover:bg-black"
+                        title="Edit"
                       >
-                        info
+                        <FaCircleInfo className="size-6" />
                       </Link>
                     </div>
                   </td>
@@ -164,7 +172,7 @@ const Inventaris = () => {
         </div>
       </main>
     </content>
-   );
+  );
 };
 
 export default Inventaris;
