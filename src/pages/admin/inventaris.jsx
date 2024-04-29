e502750a863f359dcccf17fc3bd95599a595900a
clauwaesenne <clauwaesenne@gmail.com>
2024-04-29 16:30:13 +0200

    Inventaris Tabel

--- src/pages/admin/inventaris.jsx
+++ src/pages/admin/inventaris.jsx
@@ -7,6 +7,7 @@ import { MdOutlineAddCircle } from "react-icons/md";
 import { IoMdArrowDropdown } from "react-icons/io";
 import axios from "axios";
 import { data } from "autoprefixer";
+import canonFoto from "../../assets/canon-eos-200d.jpg";
 
 const Inventaris = () => {
   const [productModellen, setProductModellen] = useState([]);
@@ -22,145 +23,147 @@ const Inventaris = () => {
         setLoading(false);
       })
       .catch((error) => {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching data: ", error);
         setLoading(false);
       });
   }, []); // Empty dependency array to run effect only once on component mount
 
   return (
-    <div> </div>
-  //   // <content className="flex top-0 flex-grow">
-  //   //   <main className="flex-grow p-10">
-  //   //     <div className="flex justify-between items-center">
-  //   //       <h1 className=" flex text-3xl font-bold w-40 border-b justify-center">
-  //   //         Inventaris
-  //   //       </h1>
-  //   //       <Link
-  //   //         to={`/admin/Inventaris/toevoegen`}
-  //   //         className="w-48 rounded-xl bg-Groen h-12 items-center justify-center flex gap-2 p-2"
-  //   //       >
-  //   //         <MdOutlineAddCircle className="flex size-6" />
-  //   //         <h2 className="font-semibold">Product Toevoegen</h2>
-  //   //       </Link>
-  //   //     </div>
-  //   //     <div className="flex items-center gap-2 mt-10 ml-5  justify-between">
-  //   //       <breadcrumb className="flex items-center gap-2">
-  //   //         <RxDashboard className="text-rood" />
-  //   //         <breadcrumb-item>Inventaris</breadcrumb-item>
-  //   //       </breadcrumb>
-  //   //       <div className="items-center flex h-12 gap-4">
-  //   //         <div className="items-center flex h-full border-2 w-56 gap-2 rounded-xl border-Lichtgrijs">
-  //   //           <IoSearchOutline className="ml-2 size-6" />
-  //   //           <input
-  //   //             type="search"
-  //   //             name=""
-  //   //             id=""
-  //   //             placeholder="Zoek hier"
-  //   //             className="h-full w-full rounded-xl"
-  //   //           />
-  //   //         </div>
-  //   //         <div className="flex h-full border-2 items-center justify-center gap-3 rounded-xl border-Lichtgrijs w-28 hover:cursor-pointer">
-  //   //           <FaFilter className="size-4 text-black-600" />
-  //   //           <h2 className="text-xl font-semibold">Filter</h2>
-  //   //         </div>
-  //   //       </div>
-  //   //     </div>
-  //   //     <div>
-  //   //       <table className="w-full border mt-4  ">
-  //   //         <thead className="w-full h-full justify-end">
-  //   //           <tr className="text-sm text-Grijs font-thin">
-  //   //             <th
-  //   //               scope="col"
-  //   //               className="py-3 px-6 w-hover:cursor-pointer border"
-  //   //             >
-  //   //               Nr
-  //   //             </th>
-  //   //             <th
-  //   //               scope="col"
-  //   //               className="py-3 px-6 hover:cursor-pointer border"
-  //   //             >
-  //   //               Product
-  //   //             </th>
-  //   //             <th scope="col" className="py-3 px-6">
-  //   //               Categorie
-  //   //             </th>
-  //   //             <th scope="col" className="py-3 px-6 flex items-center gap-1">
-  //   //               Beschikbaar
-  //   //               <IoMdArrowDropdown className="size-4" />
-  //   //             </th>
-  //   //             <th scope="col" className="py-3 px-6">
-  //   //               Uitgeleend
-  //   //             </th>
-  //   //             <th scope="col" className="py-3 px-6">
-  //   //               Gereserveerd
-  //   //             </th>
-  //   //             <th scope="col" className="py-3 px-6">
-  //   //               Gepauzeerd
-  //   //             </th>
-  //   //             <th scope="col" className="py-3 text-right">
-  //   //               Actie
-  //   //             </th>
-  //   //           </tr>
-  //   //         </thead>
-  //   //         <tbody>
-  //   //           {orders.map((productModel, index) => (
-  //   //             <tr
-  //   //               key={productModel.productModelNr}
-  //   //               className="bg-white border-b hover:bg-gray-50"
-  //   //             >
-  //   //               <td className="py-4 px-6">{index + 1}</td>
-  //   //               <td className="py-4 px-6 items-center">
-  //   //                 <div>
-  //   //                   <img
-  //   //                     src={productModel.image}
-  //   //                     alt={productModel.name}
-  //   //                     className="rounded-mb h-full w-auto object-cover"
-  //   //                   />
-  //   //                   <h2>
-  //   //                     {productModel.name}
-  //   //                   </h2>
-  //   //                 </div>
-  //   //               </td>
-  //   //               <td className="py-4 px-6">
-  //   //                 <div>
-  //   //                   <h2>{productModel.categorie}</h2>
-  //   //                 </div>
-  //   //               </td>
-  //   //               <td className="py-4 px-6">
-  //   //                 {productModel.beschikbaar.aantal}
-  //   //               </td>
-  //   //               <td className="py-4 px-6">
-  //   //                 {productModel.uitgeleend.aantal}
-  //   //               </td>
-  //   //               <td className="py-4 px-6">
-  //   //                 {productModel.gereserveerd.aantal}
-  //   //               </td>
-  //   //               <td className="py-4 px-6">
-  //   //                 {productModel.gepauzeerd.aantal}
-  //   //               </td>
-  //   //               <td className="py-4 px-6">
-  //   //                 <div>
-  //   //                   <Link
-  //   //                     to={`/admin/inventaris/wijzigen/${productModel.id}`}
-  //   //                     className="bg-red-500 hover:bg-red-900 text-white py-2 px-4 rounded-lg"
-  //   //                   >
-  //   //                     wijzig
-  //   //                   </Link>
-  //   //                   <Link
-  //   //                     to={`/admin/inventaris/info/${productModel.id}`}
-  //   //                     className="bg-red-500 hover:bg-red-900 text-white py-2 px-4 rounded-lg"
-  //   //                   >
-  //   //                     info
-  //   //                   </Link>
-  //   //                 </div>
-  //   //               </td>
-  //   //             </tr>
-  //   //           ))}
-  //   //         </tbody>
-  //   //       </table>
-  //   //     </div>
-  //   //   </main>
-  //   // </content>
+    <content className="flex top-0 flex-grow">
+      <main className="flex-grow p-10">
+        <div className="flex justify-between items-center">
+          <h1 className=" flex text-3xl font-bold w-40 border-b justify-center">
+            Inventaris
+          </h1>
+          <Link
+            to={`/admin/Inventaris/toevoegen`}
+            className="w-48 rounded-xl bg-Groen h-12 items-center justify-center flex gap-2 p-2"
+          >
+            <MdOutlineAddCircle className="flex size-6" />
+            <h2 className="font-semibold">Product Toevoegen</h2>
+          </Link>
+        </div>
+        <div className="flex items-center gap-2 mt-10 ml-5  justify-between">
+          <breadcrumb className="flex items-center gap-2">
+            <RxDashboard className="text-rood" />
+            <breadcrumb-item>Inventaris</breadcrumb-item>
+          </breadcrumb>
+          <div className="items-center flex h-12 gap-4">
+            <div className="items-center flex h-full border-2 w-56 gap-2 rounded-xl border-Lichtgrijs">
+              <IoSearchOutline className="ml-2 size-6" />
+              <input
+                type="search"
+                name=""
+                id=""
+                placeholder="Zoek hier"
+                className="h-full w-full rounded-xl"
+              />
+            </div>
+            <div className="flex h-full border-2 items-center justify-center gap-3 rounded-xl border-Lichtgrijs w-28 hover:cursor-pointer">
+              <FaFilter className="size-4 text-black-600" />
+              <h2 className="text-xl font-semibold">Filter</h2>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-full h-screen">
+          <table className="w-full mt-4 h-screen  ">
+            <thead className="w-full h-16">
+              <tr className="text-sm text-Grijs font-thin ">
+                <th
+                  scope="col"
+                  className="py-3 px-6 w-hover:cursor-pointer "
+                >
+                  Nr
+                </th>
+                <th
+                  scope="col"
+                  className="py-3 px-6 hover:cursor-pointer"
+                >
+                  Product
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Categorie
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Beschikbaar
+                  <IoMdArrowDropdown className="size-4" />
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Uitgeleend
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Gereserveerd
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Gepauzeerd
+                </th>
+                <th scope="col" className="py-3 text-right">
+                  Actie
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {productModellen.map((model) => (
+                <tr
+                  key={model.productModelNr}
+                  className="border"
+                >
+                  <td className="flex justify-center">{model.productModelNr}</td>
+                  <td className=" items-center">
+                    <div className="flex h-full item-center">
+                      <div className="h-16 auto">
+                      <img
+                        src={canonFoto}
+                        alt=""
+                        className=""
+                      />
+                      </div>
+                      <h2 className="flex flex-col">
+                        <span>{`${model.productModelMerk}`}</span>
+                        <span>{`${model.productModelNaam}`}</span>
+                      </h2>
+                    </div>
+                  </td>
+                  <td className="">
+                    <div>
+                      <h2>{model.categorie}</h2>
+                    </div>
+                  </td>
+                  <td className="">
+                    {model.beschikbaar}
+                  </td>
+                  <td className="">
+                    {model.uitgeleend}
+                  </td>
+                  <td className="">
+                    {model.gereserveerd}
+                  </td>
+                  <td className="">
+                    {model.gepauzeerd}
+                  </td>
+                  <td className="">
+                    <div>
+                      <Link
+                        to={`/admin/inventaris/wijzigen/${model.productModelNr}`}
+                        className="bg-red-500 hover:bg-red-900 text-white py-2 px-4 rounded-lg"
+                      >
+                        wijzig
+                      </Link>
+                      <Link
+                        to={`/admin/inventaris/info/${model.productModelNr}`}
+                        className="bg-red-500 hover:bg-red-900 text-white py-2 px-4 rounded-lg"
+                      >
+                        info
+                      </Link>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </main>
+    </content>
    );
 };
 
